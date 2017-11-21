5474a2c9c9fd7f1b6c2944b70972fbced426bed2
NattawutW <nattawut-39@hotmail.com>
2017-11-21 15:19:49 +0700

    testing add data

--- src/ooptest/HelloWorld.java
+++ src/ooptest/HelloWorld.java
@@ -3,7 +3,11 @@ package ooptest;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World, I'm GUN");
-
+//        Add Data
+        System.out.println("Nattawut Wattananil");
+        System.out.println("Information System");
+        System.out.println("Faculty of Management Technology");
+        System.out.println("RMUTSV");
     }//main
 
 }//class

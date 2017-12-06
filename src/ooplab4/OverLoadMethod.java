2d545b6abc6e33745fa6cec756f2d82a20433b3f
NattawutG <nattawut-39@hotmail.com>
2017-12-06 11:35:57 +0700

    OverLoadMethod

---
+++ src/ooplab4/OverLoadMethod.java
@@ -0,0 +1,16 @@
+package ooplab4;
+//Overloading method
+//Overload method
+//Method Overload
+
+public class OverLoadMethod {
+    private static int number = 100;
+    public static void sum (int x,int y){
+        System.out.println(x + y);
+        int t = number + x + y;
+    }
+
+    public static void main(String[] args) {
+
+    }//main
+}//class

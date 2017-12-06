476f06bf8bd756daf0e6ea87cbe59f9234079bb6
NattawutG <nattawut-39@hotmail.com>
2017-12-06 09:47:44 +0700

    Testing Loop While

---
+++ src/ooplab3/TestDoWhileLoop.java
@@ -0,0 +1,13 @@
+package ooplab3;
+
+public class TestDoWhileLoop {
+    public static void main(String[] args) {
+//        do-while
+        int n = 1;
+        do {
+//                statement
+            System.out.print(n + " ");
+        } while (++n <= 10);
+    }//main
+
+}//class

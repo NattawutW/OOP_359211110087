578aa27790421a2f5c40e257505ee2e86882747d
NattawutG <nattawut-39@hotmail.com>
2017-12-06 10:09:51 +0700

    testdowhile

--- src/ooplab3/TestDoWhileLoop.java
+++ src/ooplab3/TestDoWhileLoop.java
@@ -1,13 +1,36 @@
 package ooplab3;
 
+import java.util.Scanner;
+
 public class TestDoWhileLoop {
     public static void main(String[] args) {
 //        do-while
-        int n = 1;
-        do {
-//                statement
-            System.out.print(n + " ");
-        } while (++n <= 10);
-    }//main
+//        int n = 1;
+//        do {
+////                statement
+//            System.out.print(n + " ");
+//        } while (++n <= 10);
 
+//    password correct
+
+    int pin = 1234;
+    int count = 0;
+    int input;
+    Scanner scanner = new Scanner(System.in);
+    do
+
+    {
+        System.out.print("Enter your password :");
+        input = scanner.nextInt();
+        count++;
+        if (input ==pin) {
+            System.out.println("Your password is correct.");
+            break;
+        }
+        if (count ==3) {
+            System.out.println("System error");
+            break;
+        }
+    }while(input !=pin);
+}//main
 }//class

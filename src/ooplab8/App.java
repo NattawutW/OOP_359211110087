8534c9467a8978e25d992f45a8c0c38688915240
NattawutGN <nattawut-39@hotmail.com>
2018-01-31 11:18:52 +0700

    Inheritance : interface Shape

---
+++ src/ooplab8/App.java
@@ -0,0 +1,9 @@
+package ooplab8;
+
+public class App {
+    public static void main(String[] args) {
+        triangle triangle = new triangle(100, 50);
+        System.out.println(triangle.toString());
+        System.out.println(triangle.getArea());
+    }
+}

23be32fd7ba71dde1907892f3843dc781bdab9be
NattawutGN <nattawut-39@hotmail.com>
2018-01-31 10:27:21 +0700

    Inheritance : employee

---
+++ src/ooplab8/EmployeeApp.java
@@ -0,0 +1,8 @@
+package ooplab8;
+
+public class EmployeeApp {
+    public static void main(String[] args) {
+        Employee employee = new Employee("Nattawut","MALE","07/08/1996",22,"MANAGER",50000);
+     System.out.println(employee.toString());
+    }
+}//class
